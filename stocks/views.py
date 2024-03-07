d57374420bd5d3cf322aef420884f60f0e4345d2
Ubuntu <ubuntu@ip-172-31-40-89.ec2.internal>
2024-03-07 12:18:22 +0000

    Latest commit

--- stocks/views.py
+++ stocks/views.py
@@ -4,6 +4,17 @@ from yahoo_fin.stock_info import *
 # Create your views here.
 def home(request):
     stock_list = tickers_nifty50()
+    
+    new_stocks = ['ADANIPORTS.NS', 'ASIANPAINT.NS', 'AXISBANK.NS', 'BPCL.NS', 'DIVISLAB.NS', 'DRREDDY.NS', 'EICHERMOT.NS', 
+            'GRASIM.NS', 'HCLTECH.NS', 'HDFCBANK.NS', 'HINDUNILVR.NS', 'ICICIBANK.NS', 'INFY.NS', 'JSWSTEEL.NS',
+            'POWERGRID.NS', 'SBIN.NS', 'SBILIFE.NS', 'SUNPHARMA.NS', 'TATAMOTORS.NS', 'UPL.NS']
+    
+    update_stock = stock_list.index('MM.NS')
+    stock_list[update_stock] = 'M&M.NS'
+    
+    stock_list.extend(new_stocks)
+    stock_list.sort()
+
     return render(request, 'stocks/index.html', {'stock_list':stock_list})
 
 def stocksInfo(request):
